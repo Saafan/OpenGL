932367506aa17251152ed2e692b76a82f28c1320
Abdullah Saafan <abdullah.safaan@gmail.com>
2020-10-03 22:59:13 +0200

    Added All Libraries and Abstraction Classes

---
+++ OpenGL/src/main.cpp
@@ -0,0 +1,53 @@
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+	{
+		std::cout << "GLFW Initialization Failed!" << std::endl;
+		return -1;
+	}
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(1280, 720, "OpenGL Window", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		std::cout << "GLFW Window Initialization Failed!" << std::endl;
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW Initialization Failed!" << std::endl;
+		return -1;
+	}
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2f, 0.5f, 0.8f, 1.0f);
+
+		//Code Here
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
