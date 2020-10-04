bbc088207017915a33a441e8d1ca7fb3f27b520b
Abdullah Saafan <abdullah.safaan@gmail.com>
2020-10-04 23:53:30 +0200

    Added ImGUI
    Added ImGUI Library and Ready to use ImGUI main

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -44,7 +44,7 @@ int main(void)
 		glClearColor(0.2f, 0.5f, 0.8f, 1.0f);
 
 		//Code Here
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
